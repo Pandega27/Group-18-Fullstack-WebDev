858483872fea4eb1362427d0e211310d07cda136
chipa-chipa <129848462+chipa-chipa@users.noreply.github.com>
2024-09-09 17:13:32 +0700

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import { useSelector } from 'react-redux'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 import { themeSettings } from './theme'
-import { useSelector } from 'react-redux'
 
 function App() {
   const mode = useSelector((state) => state.mode)
